2540d4aac6895d7c23944b4972b8c465b7e17219
anassar@itgsoftware.com <anassar@itgsoftware.com>
2020-07-12 16:23:28 +0300

    Updates

--- precache-manifest.b89a2695c90f808a1b9e55158f9a720d.js
+++ precache-manifest.b89a2695c90f808a1b9e55158f9a720d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a5bb9df5a62fb55d2ecea8559835316",
+    "revision": "791c3cb9c3e2697de32f882de0cbc21b",
     "url": "/robofriends/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "e28ed18023aeb26e4a80",
+    "revision": "ce6fce3a544206150b64",
     "url": "/robofriends/static/css/main.6be1a416.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e28ed18023aeb26e4a80",
-    "url": "/robofriends/static/js/main.e5dd84de.chunk.js"
+    "revision": "ce6fce3a544206150b64",
+    "url": "/robofriends/static/js/main.5a97ef22.chunk.js"
   },
   {
     "revision": "e7d2cbb368e0813da61d",
